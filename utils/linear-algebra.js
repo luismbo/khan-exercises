b70e4274c736dc1abc771bb5e69bc2977dd8be55
Luís Oliveira <loliveira@common-lisp.net>
2012-01-18 00:06:06 +0000

    adding_matrices: display intermediate steps when adding A and B.

--- utils/linear-algebra.js
+++ utils/linear-algebra.js
@@ -5,30 +5,46 @@ jQuery.extend(KhanUtil, {
 	// Computes a random matrix of size m by n, with random elements varying
 	// between min and max.
 	randMatrix: function( m, n, min, max ) {
-		return Matrix.Random( m, n ).map(function( x ){
+		return Matrix.Random( m, n ).map(function( x ) {
 			return Math.floor( (max - min + 1) * x ) + min;
 		});
 	},
 
-	// Returns TeX code to represent matrix A, an instance of type Matrix.
-	matrixTeX: function( A ) {
+	// Calls fn for each element the indexes of each element in matrix A,
+	// an instance of type Matrix. Collects each string returned by fn into
+	// TeX matrix format.
+	mapMatrixToTex: function( A, fn ) {
 		var tex = "\\begin{bmatrix}\n";
 
 		var m = A.rows(), n = A.cols();
-		for (var i = 1; i <= m; i++)
-			for (var j = 1; j <= n; j++)
-				tex += A.e( i , j ) + (j === n ? " \\\\\n" : " &amp; ");
+		for ( var i = 1; i <= m; i++ )
+			for ( var j = 1; j <= n; j++ )
+				tex += fn.call( A, i, j ) + (j === n ? " \\\\\n" : " &amp; ");
 
 		return tex + "\\end{bmatrix}";
 	},
 
-	// Returns TeX code to represent vector, an instance of type Vector. If
-	// isRowVector is true, the vector is represented as a row vector
+	// Returns TeX code to represent matrix A, an instance of type Matrix.
+	matrixTeX: function( A ) {
+		return KhanUtil.mapMatrixToTex( A, A.e );
+	},
+
+	// Returns TeX code to represent vector, an instance of type Vector.
+	// If isRowVector is true, the vector is represented as a row vector
 	// (horizontally), otherwise it is represented as a column vector
 	// (vertically).
-	matrixTeX: function ( vector, isRowVector ) {
-		var mat = Matrix.create(vector);
-		return KhanUtil.matrixTeX(isRowVector ? mat.transpose() : mat);
+	vectorTeX: function( vector, isRowVector ) {
+		var mat = Matrix.create( vector );
+		return KhanUtil.matrixTeX( isRowVector ? mat.transpose() : mat );
+	},
+
+	// Returns TeX code to display an intermediate representation of A+B
+	// where each element is represented in the form "a_ij + b_ij".
+	displayMatrixAddition: function( A, B ) {
+		Khan.assert( A.isSameSizeAs(B), "matrices A and B must have the same size." );
+		return KhanUtil.mapMatrixToTex( A, function( i, j ) {
+			return A.e( i, j ) + " + " + B.e( i, j );
+		});
 	}
 
 });
